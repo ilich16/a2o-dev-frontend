4e0cf9791168205f4c28bb706fa4e9f70abd3b3d
unknown <diego.severiche89@gmail.com>
2020-12-21 01:09:37 -0400

    Add the component for the first problem in the App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import './App.css';
+import FirstProblem from './components/FirstProblem';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/problem-1" component={FirstProblem} />
+          <Route path="/problem-2">
+            <Problem2 />
+          </Route>
+          <Route path="/problem-3">
+            <Problem3 />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Problem2() {
+  return <h2>Problem 2</h2>;
+}
+
+function Problem3() {
+  return <h2>Problem 3</h2>;
 }
 
 export default App;
